c10863c89701f8368f8a3a3c37a4ce8e15f0fdf4
Deng Liang <deng@LiangdeMacBook-Air.local>
2021-08-30 21:24:57 +0800

    增加文件上传、下载api

--- Controllers/Upload/FileController.cs
+++ Controllers/Upload/FileController.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json.Linq;
 using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetCoreWebApi.Controllers.Upload
 {
@@ -16,9 +17,11 @@ namespace DotNetCoreWebApi.Controllers.Upload
     {
         //初始化 
         public static IWebHostEnvironment _environment;
-        public FileController(IWebHostEnvironment environment)
+        CoreDbContext _Context;
+        public FileController(IWebHostEnvironment environment, CoreDbContext context)
         {
             _environment = environment;
+            _Context = context;
         }
 
         public class FileUploadAPI
@@ -26,8 +29,9 @@ namespace DotNetCoreWebApi.Controllers.Upload
             public IFormFile files { get; set; }
         }
 
+        [Authorize]
         [HttpPost]
-        public IActionResult Save([FromForm] FileUploadAPI objFile)
+        public IActionResult Save([FromForm] FileUploadAPI objFile, string UserAcc)
         {
             JObject rv = new JObject();
             JArray children = new JArray();
@@ -38,19 +42,35 @@ namespace DotNetCoreWebApi.Controllers.Upload
                     rv["success"] = true;
                     JObject item = new JObject();
                     children.Add(item);
-                    string path = _environment.WebRootPath + "Upload";
+                    string path = _environment.WebRootPath + "Upload/" + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Day;
                     if (!Directory.Exists(path))
                     {
                         Directory.CreateDirectory(path);
                     }
                     string FileName = objFile.files.FileName;
                     string Ext = FileName.Substring(FileName.LastIndexOf('.'), FileName.Length - FileName.LastIndexOf('.'));
-                    using (FileStream filesStream = System.IO.File.Create(path + "/" + objFile.files.FileName))
+                    //文件名称使用guid生成
+                    string FileID = Guid.NewGuid().ToString();
+                    using (FileStream filesStream = System.IO.File.Create(path + "/" + FileID + Ext))
                     {
                         objFile.files.CopyTo(filesStream);
+                        //保存文件信息到数据库
+                        Attachments att = new Attachments()
+                        {
+                            FileName = FileName,
+                            FileID = FileID,
+                            Size = objFile.files.Length,
+                            FilePath = path,
+                            CreateUser = UserAcc,
+                            CreateDate = DateTime.Now,
+                            Ext = Ext
+                        };
+                        _Context.Attachments.Add(att);
+                        _Context.SaveChanges();
                         filesStream.Flush();
-                        item["FilePath"] = path + "/" + objFile.files.FileName;
+                        item["FilePath"] = path + "/" + FileID + Ext;
                         item["FileName"] = FileName;
+                        item["FileID"] = FileID;
                         item["Ext"] = Ext;
                         item["Size"] = objFile.files.Length;
                         rv["data"] = children;
@@ -70,10 +90,41 @@ namespace DotNetCoreWebApi.Controllers.Upload
             return Ok(rv.ToString());
         }
 
-        public IActionResult Download([FromForm] string FileID)
+        [HttpGet]
+        public IActionResult Download(string FileID)
         {
-            return null;
+            JObject rv = new JObject();
+            if (!string.IsNullOrEmpty(FileID))
+            {
+                var data = _Context.Attachments.Where(b => b.FileID == FileID).ToList();
+                if (data.Count() > 0)
+                {
+                    string filePath = AppContext.BaseDirectory + data[0].FilePath + "/" + data[0].FileID + data[0].Ext;
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+                        return File(fileStream, "application/octet-stream", data[0].FileName + data[0].Ext);
+                    }
+                    else
+                    {
+                        rv["success"] = false;
+                        rv["msg"] = "文件不存在或已被删除";
+                        return Ok(rv.ToString());
+                    }
+                }
+                else
+                {
+                    rv["success"] = false;
+                    rv["msg"] = "FIleID不存在";
+                    return Ok(rv.ToString());
+                }
+            }
+            else
+            {
+                rv["success"] = false;
+                rv["msg"] = "FIleID不能为空";
+                return Ok(rv.ToString());
+            }
         }
-        
     }
 }
