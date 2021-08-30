cb4fc014284bdd10fa83a5dc5b0dfdaa2ed3edce
Deng Liang <deng@LiangdeMacBook-Air.local>
2021-08-30 21:13:18 +0800

    增加MD5加密类库

---
+++ App_Code/Common/MD5.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DotNetCoreWebApi
+{
+    public class MD5H
+    {
+        /// <summary>
+        /// MD5加密字符串（32位大写）
+        /// </summary>
+        /// <param name="source">源字符串</param>
+        /// <returns>加密后的字符串</returns>
+        public static string MD5(string source)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = Encoding.UTF8.GetBytes(source);
+            string result = BitConverter.ToString(md5.ComputeHash(bytes));
+            return result.Replace("-", "");
+        }
+    }
+}
